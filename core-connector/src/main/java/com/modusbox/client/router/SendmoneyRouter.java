5efb4ac3d13b857daa2436aa891e49c654cd8ee0
Yordan Stoykov <yordan.stoykov@modusbox.com>
2021-08-23 17:02:47 +0300

    Added /postsendmoney endpoint

--- core-connector/src/main/java/com/modusbox/client/router/SendmoneyRouter.java
+++ core-connector/src/main/java/com/modusbox/client/router/SendmoneyRouter.java
@@ -33,11 +33,10 @@ public class SendmoneyRouter extends RouteBuilder {
     private final RouteExceptionHandlingConfigurer exceptionHandlingConfigurer = new RouteExceptionHandlingConfigurer();
 
     public void configure() {
-
         // Add our global exception handling strategy
         exceptionHandlingConfigurer.configureExceptionHandling(this);
 
-        from("direct:postSendMoney").routeId("com.modusbox.postSendMoney").doTry()
+        from("direct:postSendMoney").routeId("com.modusbox.postSendMoney")
                 .process(exchange -> {
                     reqCounterPost.inc(1); // increment Prometheus Counter metric
                     exchange.setProperty(TIMER_NAME_POST, reqLatencyPost.startTimer()); // initiate Prometheus Histogram metric
@@ -45,39 +44,65 @@ public class SendmoneyRouter extends RouteBuilder {
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Request received, POST /sendmoney', " +
                         "null, null, 'Input Payload: ${body}')")
-                /*
-                 * BEGIN processing
-                 */
                 .setProperty("origPayload", simple("${body}"))
                 .removeHeaders("CamelHttp*")
                 .setHeader(Exchange.HTTP_METHOD, constant("POST"))
                 .setHeader("Content-Type", constant("application/json"))
+
+                // Prune empty items from the request
+                .marshal().json()
+                .transform(datasonnet("resource:classpath:mappings/postSendMoneyRequest.ds"))
+                .setBody(simple("${body.content}"))
+                .marshal().json()
+
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Calling outbound API, postTransfers, " +
                         "POST {{ml-conn.outbound.host}}', " +
                         "'Tracking the request', 'Track the response', 'Input Payload: ${body}')")
-                .marshal().json(JsonLibrary.Gson)
-                .toD("{{ml-conn.outbound.host}}/transfers?bridgeEndpoint=true&throwExceptionOnFailure=false")
-                .unmarshal().json(JsonLibrary.Gson)
-                /*
-                 * END processing
-                 */
+
+                .toD("{{ml-conn.outbound.host}}/transfers?bridgeEndpoint=true")
+                .unmarshal().json()
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Response from outbound API, postTransfers: ${body}', " +
                         "'Tracking the response', 'Verify the response', null)")
-                .doFinally().process(exchange -> {
+//.process(exchange -> System.out.println())
+//            .setProperty("postSendMoneyInitial", body())
+                // Send request to accept the party instead of hard coding AUTO_ACCEPT_PARTY: true
+//            .to("direct:putTransfersAcceptParty")
+                .process(exchange -> {
                     ((Histogram.Timer) exchange.getProperty(TIMER_NAME_POST)).observeDuration(); // stop Prometheus Histogram metric
-                }).end()
+                })
         ;
 
-        from("direct:putSendMoneyByTransferId").routeId("com.modusbox.putSendMoneyByTransferId").doTry()
+        from("direct:putTransfersAcceptParty")
+                .routeId("com.modusbox.putTransfersAcceptParty")
+                .removeHeaders("CamelHttp*")
+                .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
+                .setHeader("Content-Type", constant("application/json"))
+
+                .marshal().json()
+                .transform(datasonnet("resource:classpath:mappings/putTransfersAcceptPartyRequest.ds"))
+                .setBody(simple("${body.content}"))
+                .marshal().json()
+
+                .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+                        "'Calling outbound API, putTransfersAcceptParty, " +
+                        "PUT {{ml-conn.outbound.host}}/transfers/${exchangeProperty.postSendMoneyInitial?.get('transferId')}', " +
+                        "'Tracking the request', 'Track the response', 'Input Payload: ${body}')")
+//                .marshal().json()
+                // Instead of having to do a DataSonnet transformation
+                .toD("{{ml-conn.outbound.host}}/transfers/${exchangeProperty.postSendMoneyInitial?.get('transferId')}?bridgeEndpoint=true")
+                .unmarshal().json()
+                .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+                        "'Response from outbound API, putTransfersAcceptParty: ${body}', " +
+                        "'Tracking the response', 'Verify the response', null)")
+        ;
+
+        from("direct:putSendMoneyByTransferId").routeId("com.modusbox.putSendMoneyByTransferId")
                 .process(exchange -> {
                     reqCounterPut.inc(1); // increment Prometheus Counter metric
                     exchange.setProperty(TIMER_NAME_PUT, reqLatencyPut.startTimer()); // initiate Prometheus Histogram metric
                 })
-                /*
-                 * BEGIN processing
-                 */
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Request received, PUT /sendmoney/${header.transferId}', " +
                         "null, null, 'Input Payload: ${body}')")
@@ -85,22 +110,26 @@ public class SendmoneyRouter extends RouteBuilder {
                 .removeHeaders("CamelHttp*")
                 .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
                 .setHeader("Content-Type", constant("application/json"))
+
+                // Will convert to JSON and only take the accept quote section
+                .marshal().json()
+                .transform(datasonnet("resource:classpath:mappings/putTransfersAcceptRequest.ds"))
+                .setBody(simple("${body.content}"))
+                .marshal().json()
+
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Calling outbound API, putTransfersById', " +
                         "'Tracking the request', 'Track the response', " +
-                        "'Request sent to PUT https://{{ml-conn.outbound.host}}/transfers/${header.transferId}')")
-                .marshal().json(JsonLibrary.Gson)
-                .toD("{{ml-conn.outbound.host}}/transfers/${header.transferId}?bridgeEndpoint=true&throwExceptionOnFailure=false")
-                .unmarshal().json(JsonLibrary.Gson)
+                        "'Request sent to PUT {{ml-conn.outbound.host}}/transfers/${header.transferId}')")
+//                .marshal().json()
+                .toD("{{ml-conn.outbound.host}}/transfers/${header.transferId}?bridgeEndpoint=true")
+                .unmarshal().json()
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Response from outbound API, putTransfersById: ${body}', " +
                         "'Tracking the response', 'Verify the response', null)")
-                /*
-                 * END processing
-                 */
-                .doFinally().process(exchange -> {
+                .process(exchange -> {
                     ((Histogram.Timer) exchange.getProperty(TIMER_NAME_PUT)).observeDuration(); // stop Prometheus Histogram metric
-                }).end()
+                })
         ;
     }
 }
