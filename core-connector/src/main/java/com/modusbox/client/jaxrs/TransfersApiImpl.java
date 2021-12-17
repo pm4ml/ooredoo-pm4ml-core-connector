3f18f192a5c1d6b516bbc344e4a60b5ab467386d
Juan Correa <jcorreapr@gmail.com>
2021-12-16 23:41:05 -0500

    mmd-1320: Add GET transfers for given transferId, to query the Hub for the mojaloop transfer state

--- core-connector/src/main/java/com/modusbox/client/jaxrs/TransfersApiImpl.java
+++ core-connector/src/main/java/com/modusbox/client/jaxrs/TransfersApiImpl.java
@@ -2,6 +2,7 @@ package com.modusbox.client.jaxrs;
 
 import com.modusbox.client.api.TransfersApi;
 import com.modusbox.client.model.FulfilNotification;
+import com.modusbox.client.model.MojaloopTransferState;
 import com.modusbox.client.model.TransferRequestInbound;
 import com.modusbox.client.model.TransferResponseInbound;
 
@@ -20,5 +21,9 @@ public class TransfersApiImpl implements TransfersApi {
         return null;
     }
 
+    @Override
+    public MojaloopTransferState getTransfersByTransferId(@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$") String transferId) {
+        return null;
+    }
 
 }
